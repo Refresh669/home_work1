b5d549bf9604b9e54f3ccbfee484746c773b110b
refresh669 <vlad27171@gmail.com>
2023-03-02 18:13:33 +0200

    sum

---
+++ part two/script.js
@@ -0,0 +1,30 @@
+// Task #1
+
+let objectTest = {}
+objectTest.name = 'John'
+objectTest.surname = 'Smith'
+objectTest.name = 'Pete'
+delete objectTest.name
+
+// Task #2
+// Можно, потому что мы иземеняем внутренности обьета, а не сам обьект.
+const user = {
+    name: 'John'
+}
+user.name = 'Pete'
+console.log(user.name)
+
+// Task #3
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+const sum = salaries.John + salaries.Ann + salaries.Pete
+
+console.log(sum)
+
+
+
+
